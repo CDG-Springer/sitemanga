4d87945ba5efd107c5c8c1ff37e991fe49c704b0
Eric Springer <ericspringer@outlook.com>
2024-02-13 17:23:41 -0300

    função funcionando

--- loading.js
+++ loading.js
@@ -1,13 +1,13 @@
-// const loadingContainer = document.getElementById('loadingContainer');
+const loadingContainer = document.getElementById('loadingContainer');
 
-// function addHiddenLoading() {
-//     loadingContainer.style.display = 'none';
+function addHiddenLoading() {
+    loadingContainer.style.display = 'none';
 
-//     // Remova a classe 'overflow-hidden' do corpo quando o loadingContainer estiver oculto
-//     document.body.classList.remove('overflow-hidden');
-// }
+    // Remova a classe 'overflow-hidden' do corpo quando o loadingContainer estiver oculto
+    document.body.classList.remove('overflow-hidden');
+}
 
-// setInterval(addHiddenLoading, 3000);
+setInterval(addHiddenLoading, 3000);
 
-// // Adicione a classe 'overflow-hidden' ao corpo quando o loadingContainer estiver visível
-// document.body.classList.add('overflow-hidden');
+// Adicione a classe 'overflow-hidden' ao corpo quando o loadingContainer estiver visível
+document.body.classList.add('overflow-hidden');
