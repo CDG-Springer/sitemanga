12be7e6308939aee34862d875f93f35e712d7d7d
Eric Springer <ericspringer@outlook.com>
2024-02-13 16:58:46 -0300

    começo do loading

---
+++ loading.js
@@ -0,0 +1,13 @@
+// const loadingContainer = document.getElementById('loadingContainer');
+
+// function addHiddenLoading() {
+//     loadingContainer.style.display = 'none';
+
+//     // Remova a classe 'overflow-hidden' do corpo quando o loadingContainer estiver oculto
+//     document.body.classList.remove('overflow-hidden');
+// }
+
+// setInterval(addHiddenLoading, 3000);
+
+// // Adicione a classe 'overflow-hidden' ao corpo quando o loadingContainer estiver visível
+// document.body.classList.add('overflow-hidden');
